4ff858a91d3b9044aedd17d24e50605a6699f2fd
rodrigosuelli <rodrigosuelli@gmail.com>
2023-11-07 19:38:57 -0300

    Initial changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={`pageContainer ${styles.homePage}`}>
-      <h1>Ola</h1>
-      <p>Ola mundo</p>
+      <h1>Detecção facial</h1>
+      <p>Detectando aluno...</p>
+      <video id="video" width="720" height="560" autoPlay muted></video>
     </div>
   );
 }
